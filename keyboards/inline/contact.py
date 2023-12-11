772f0c6ad4e06da500f14f016fcac80e8b505b1f
qarluqturk <umidjonuchqunugli@hotmail.com>
2023-12-11 16:23:11 +0500

    completed

--- keyboards/inline/contact.py
+++ keyboards/inline/contact.py
@@ -8,7 +8,7 @@ def contact():
     instagram_button = InlineKeyboardButton("📸Instagram", url='https://www.instagram.com/p/CzOL3A5t9gA/')
     tiktok_button = InlineKeyboardButton("📱TikTok", url='http://www.tiktok.com/@afc_food')
     website_button = InlineKeyboardButton("💻Website", url='https://www.afc-foods.uz/')
-    location_button = InlineKeyboardButton("📍Manzil", url='https://goo.gl/maps/6qWwY5HtJYhJxHrC8')
+    location_button = InlineKeyboardButton("📍Manzil", url='https://www.google.com/maps/place/Clever+qarshi/@38.8368838,65.779254,17z/data=!3m1!4b1!4m6!3m5!1s0x3f4ea98e1091f4e3:0x14ffce588cd416b9!8m2!3d38.8368838!4d65.7818289!16s%2Fg%2F11r_np7yzk?entry=ttu')
     back_button = InlineKeyboardButton("⬅️Orqaga", callback_data='back')
 
     # Add buttons to the keyboard
